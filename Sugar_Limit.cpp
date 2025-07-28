dc99e4f19e7cd40f6bea3d99efb056bff02a2e3e
suruzzaman <mdsuruzzaman134@gmail.com>
2025-07-28 19:02:55 +0600

    solve one old problem through different approch

--- Sugar_Limit.cpp
+++ Sugar_Limit.cpp
@@ -1,35 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main(){
-   ios::sync_with_stdio(false);
-   cin.tie(nullptr);
 
-   int t; cin >> t;
-   while (t--)
-   {
-      int n; cin >> n;
-      vector<int> v(n), b(n);
-      for(int i = 0; i < n; i++) cin >> v[i];
+int main() {
+	int t; cin >> t;
+	while(t--){
+	    int n; cin >> n;
+	    vector<int> a(n), b(n);
+	    
+	    for(int i = 0; i < n; i++) cin >> a[i];
+	    for(int i = 0; i < n; i++) cin >> b[i];
+	    
+	    int max_t = INT_MIN;
+	    
+	    for(int i = 0; i < n; i++){
+	        int current_t = 0;
+	        
+	        for(int j = 0; j < n; j++){
+	            if(b[j] <= b[i] && a[j] > 0){
+	                current_t += a[j];
+	            }
+	        }
+	        
+	        max_t = max(max_t, current_t - b[i]);
+	        
+	    }
+	    
+	    cout << max(max_t, 0) << "\n";
+	}
 
-      for(int i = 0; i < n; i++){
-         cin >> b[i];
-      }
-      int max_satisfaction = 0;
-      for(int l : b){
-         int sum = 0;
-
-         for(int i = 0; i < n; i++){
-            if(b[i] <= l && v[i] > 0){
-               sum += v[i];
-            }
-         }
-
-         max_satisfaction = max(max_satisfaction, sum - l);
-      }
-
-      cout << max_satisfaction << "\n";
-   }
-   
-
-   return 0;
 }
