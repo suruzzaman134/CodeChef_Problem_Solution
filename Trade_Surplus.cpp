21773a715afa9fb5dac576583ab408f45e8a7383
suruzzaman <mdsuruzzaman134@gmail.com>
2025-09-17 18:21:21 +0600

    solve one more problem

--- Trade_Surplus.cpp
+++ Trade_Surplus.cpp
@@ -1,1 +1,18 @@
 // Problem Link : https://www.codechef.com/problems/SURPLUS
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t; cin >> t;
+   while (t--)
+   {
+      int a, b, c, d; cin >> a >> b >> c >> d;
+      if(b + d > a + c) cout << "YES" << "\n";
+      else cout << "NO" << "\n";
+   }
+   
+
+   return 0;
+}
