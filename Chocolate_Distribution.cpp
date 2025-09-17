1a9443a83568ffb429f3caeee6cb560574edc8ce
suruzzaman <mdsuruzzaman134@gmail.com>
2025-09-17 18:38:21 +0600

    solve one more 1100 rated problem in codechef

---
+++ Chocolate_Distribution.cpp
@@ -0,0 +1,17 @@
+// Problem Link : https://www.codechef.com/problems/CHOCOCHEF
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t; cin >> t;
+   while (t--)
+   {
+      int n; cin >> n;
+      cout << 1 << " " << 1 << " " << n - 2 << "\n";
+   }
+   
+
+   return 0;
+}
