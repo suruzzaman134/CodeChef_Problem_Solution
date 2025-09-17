408baa33d56ee1c6c9e7dfd583d971c177d22835
suruzzaman <mdsuruzzaman134@gmail.com>
2025-09-17 18:19:21 +0600

    solve one more problem

---
+++ Air_Quality_Index.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/AIRINDEX
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int n; cin >> n;
+   if(n < 100) cout << "YES";
+   else cout << "NO";
+
+   return 0;
+}
