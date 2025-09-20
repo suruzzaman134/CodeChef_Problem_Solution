f9c93853577df46eb1f7dee7bbc261d0a5884842
suruzzaman <mdsuruzzaman134@gmail.com>
2025-09-20 18:56:23 +0600

    solve one more 700 rated problem

---
+++ Cyclic_Quadrilateral.cpp
@@ -0,0 +1,18 @@
+// Problem Link : https://www.codechef.com/practice/course/logical-problems/DIFF800/problems/CYCLICQD
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t; cin >> t;
+   while (t--)
+   {
+      int a, b, c, d; cin >> a >> b >> c >> d;
+      if(a + c >= 180 && b + d >= 180) cout << "YES" << "\n";
+      else cout << "NO" << "\n";
+   }
+   
+
+   return 0;
+}
