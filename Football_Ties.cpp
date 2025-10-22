d3f47e6f8bcdb367429cb992b32f34cc82b842cc
suruzzaman <mdsuruzzaman134@gmail.com>
2025-10-22 17:15:49 +0600

    solve two more problem in codechef

---
+++ Football_Ties.cpp
@@ -0,0 +1,18 @@
+// Problem Link : https://www.codechef.com/problems/FOOTBALLTIES
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t; cin >> t;
+   while (t--)
+   {
+      int x, y; cin >> x >> y;
+
+      cout << x % 3 << "\n";
+   }
+   
+
+   return 0;
+}
---
+++ Invert_And_Equalize.cpp
@@ -0,0 +1,24 @@
+// Problem Link : https://www.codechef.com/problems/INVEQ
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+   ios::sync_with_stdio(false);
+   cin.tie(nullptr);
+
+   int t; cin >> t;
+   while (t--)
+   {
+      int n; cin >> n;
+      string s; cin >> s;
+
+      int cnt = 0;
+      for(int i = 1; i < n; i++){
+         if(s[i] != s[i-1]) cnt++;
+      }
+
+      cout << (cnt + 1) / 2 << "\n";
+   }
+   
+
+   return 0;
+}
