2e370c140c5e04020e88c65af6135af930119be1
suruzzaman <mdsuruzzaman134@gmail.com>
2025-11-13 10:08:45 +0600

    solve one more codechef problem

---
+++ Distinct_Fun.cpp
@@ -0,0 +1,43 @@
+// Problem Link : https://www.codechef.com/problems/P7BAR?tab=statement
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+typedef vector<int> vi;
+#define vec_in(v) for(int i = 0; i < v.size(); ++i) cin >> v[i];
+#define vec_out(v) for(int i = 0; i < v.size(); ++i) cout << v[i] << " ";
+
+void aditya46raj() {
+    // Let's Go Aditya you can do it
+    int ans=0;
+    int n;cin>>n;
+    vi arr(n);
+    vec_in(arr);
+    set<int>s;
+    for(int i=0;i<n;i++){
+        if(s.find(arr[i])!= s.end()){
+            s.clear();
+            ans++;
+            s.insert(arr[i]);
+        }
+        else{
+            s.insert(arr[i]);
+        }
+    }
+    
+    cout<<ans<<endl;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+
+    while (tc--) {
+        aditya46raj();
+    }
+    return 0;
+}
